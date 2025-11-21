9849e384773da6bf6ec4e005e03a9b500c767066
Jorge Perez <jorgeimon100@gmail.com>
2025-11-21 11:31:54 +0100

    (Actualizado)

---
+++ src/basics/conversiontest.java
@@ -0,0 +1,21 @@
+package basics;
+
+public class conversiontest {
+	
+	public static void main(String[] args) {
+		int num1 = 23443434;
+		byte num2 = 127;
+		float num3 = 324.2f;
+		num1 = num2;
+		
+		num2 =(byte) num1;
+		
+		
+		
+	}
+	
+	private static void primitiveConversion() {
+		
+	}
+	
+}
