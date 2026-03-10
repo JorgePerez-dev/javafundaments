2089d49d895674654ed0a5a89bbb10e95239f860
Jorge Perez <jorgeimon100@gmail.com>
2026-03-10 09:54:33 +0100

    nodos

--- src/datastructure/list/LinkedListImpl.java
+++ src/datastructure/list/LinkedListImpl.java
@@ -1,6 +1,22 @@
 package datastructure.list;
 
+
+/**
+ * List implementaion by linking node objects
+ * @author Jorge
+ * Mar 10, 2026
+ * @param <E>
+ */
+
+
 public class LinkedListImpl<E> implements List<E> {
+	
+	
+	private Node<E> first;
+	private Node<E> Last;
+	
+		
+	
 
 	@Override
 	public void add(E e) {
--- src/datastructure/list/Linkedlistest.java
+++ src/datastructure/list/Linkedlistest.java
@@ -1,19 +1,25 @@
 package datastructure.list;
 
-import oopmodeling.Planet;
-
 public class Linkedlistest {
-	
-	public static void main(String[] args) {
-		Node<Planet> first = new Node<Planet>(1000, "Earth", 14535);
-		Node<Planet> first = new Node<Planet>(1000, "Moon", 14535);
-		Node<Planet> first = new Node<Planet>(1000, "Mars", 14535);
-		
-		first.setNext(second);
-		
 
-		
-		
-	}
+    public static void main(String[] args) {
+
+        NodePlanet first = new NodePlanet(1000, "Earth", 14535);
+        NodePlanet second = new NodePlanet(1000, "Moon", 14535);
+        NodePlanet third = new NodePlanet(1000, "Mars", 14535);
+        NodePlanet fourth = new NodePlanet(1000, "Earth", 14535);
+
+
+        first.setNext(second);
+        second.setNext(third);
+        third.setNext(fourth);
+        
+        LinkedListImpl<Integer> list = new LinkedListImpl<Integer>();
+        list.add(12);
+        list.add(333);
+        list.add(20);
+        list.add(320);
+
+    }
 
 }
