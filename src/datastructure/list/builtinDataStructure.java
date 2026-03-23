ed79f934e370bc5c540471419d29038e90e9f1a4
Jorge Perez <jorgeimon100@gmail.com>
2026-03-23 13:10:05 +0100

    datastructure

---
+++ src/datastructure/list/builtinDataStructure.java
@@ -0,0 +1,27 @@
+package datastructure.list;
+
+import java.util.ArrayList;
+
+import oopmodeling.Planet;
+
+public class builtinDataStructure {
+	
+	
+	public static void main(String[] args) {
+		
+		Planet planet1 = new Planet();
+		Planet planet2 = new Planet();
+
+		ArrayList<Planet> planets = new ArrayList<>();
+		
+		planets.add(planet2);
+		planets.add(planet1);
+		
+		
+		
+		
+		
+		ArrayList<Planet> arr;
+	}
+
+}
